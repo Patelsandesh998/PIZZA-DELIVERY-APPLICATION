1378d23eb46b5999e77da1257fe254f4a0014866
Patelsandesh998 <148585012+Patelsandesh998@users.noreply.github.com>
2023-10-22 01:36:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Assuming you have an element representing the order status
+const orderStatusElement = document.querySelector('.order-status');
+
+// Get the updated order status from the server
+const updatedStatus = "in-the-kitchen"; // Example status, you should get this from the server
+
+// Update the class to apply the appropriate styles
+orderStatusElement.className = order-{updatedStatus};
